361f4f41b2496a6567f55fab04befc4520d447c2
JesseLefty <jesselefty88@gmail.com>
2021-11-30 18:50:21 -0600

    Addressed comments from 11/29 on included files
    Main changes to these files include:
    
    Added docstrings to each function
    Added argparser to run program from main
    Revised how function 'member_force_array' generates the 2d array by using np.concatenate
    Revised how function 'requested_member_force_array' generates the requested member forces array (as dictionary)
    passed arguments through functions as necessary
    combined functions where possible and returned multiple values as tuple

--- extract_member_forces.py
+++ extract_member_forces.py
@@ -1,36 +1,57 @@
-# REFORMAT THE MEMBER FORCE LIST TO FILL EACH ROW WITH REQUIRED DATA
-
 import numpy as np
 import generate_mem_array_info as g_mem
-joint = 'START'
-beam_option = 1     # options so far are "all" (1) or "starts with" (2) or "specific names" (3), want to add "ends with" and "contains"
-load_option = 1
-lc_count = g_mem.load_comb_count()
 
 
-# ENSURES THE ARRAY IS ALL THE SAME SIZE (X, 9) WHERE ALL POSITIONS OF ELEMENTS ARE RETAINED IN THEIR ORIGINAL COLUMNS
-def member_force_array():
-    member_forces = np.array(g_mem.member_forces)
-    len_first_row = len("".join(member_forces[0]).split())
+class TupleDict(dict):
+    """
+    Rebuilds a dictionary which allows searching a key of type tuple for any value. If any item in the tuple matches the
+    requested value, the entire key is returned.
+
+            Parameters:
+                dict:   dictionary which is to be converted to searchable
+
+            Returns:    a dictionary which has a searchable tuple key
+    """
+    def __contains__(self, key):
+        if super(TupleDict, self).__contains__(key):
+            return True
+        return any(key in k for k in self)
+
+
+def member_force_array(beam_option, load_option, file_list):
+    """
+    Generates list used for generating dictionary key / value pairs and a 2d array of all elements in data block
+    with empty spaces filled with applicable data in which the key / value pairs are used.
+
+        Parameters:
+            None yet - will discuss beam option and load options
+
+        Returns:
+             all_member_force (array):      2d array (x, 9) of member results with empty spaces filled with correct
+                                            member names and load combinations
+             beam (list(str)):              list of unique beam names in data block corresponding to selected
+                                            beam option
+             load (list(str)):              list of unique load combinations in data block corresponding to selected
+                                            load option
+    """
+    member_forces, lc_count, truss_member, ph_1, ph_2, ph_3 = g_mem.get_member_force_list_info(file_list)
     standard_member_force = [' ']*9
+    len_first_truss_mem = len("".join(member_forces[0]).split())
     for all_rows in range(member_forces.shape[0]):
-        raw_member_force_array = "".join(member_forces[all_rows]).split()
-        while len(raw_member_force_array) < len_first_row:
-            raw_member_force_array = np.insert(raw_member_force_array, 0, " ")
-            # print(raw_member_force_array)
-        if len(raw_member_force_array) < 9:
-            while len(raw_member_force_array) < 9:
-                raw_member_force_array = np.insert(raw_member_force_array, len(raw_member_force_array), " ")
-        else:
+        raw_member_force_array = np.array("".join(member_forces[all_rows]).split())
+        if raw_member_force_array.shape[0] == 9:
             pass
-        raw_member_force_array = raw_member_force_array
+        elif raw_member_force_array.shape[0] < 9 and not truss_member:
+            extend = 9 - raw_member_force_array.shape[0]
+            raw_member_force_array = np.concatenate((np.array([' ']*extend), raw_member_force_array), axis=0)
+        if truss_member:
+            extend_back = 9 - len_first_truss_mem
+            raw_member_force_array = np.concatenate((raw_member_force_array, np.array([' '] * extend_back)), axis=0)
+            extend_front = 9 - raw_member_force_array.shape[0]
+            raw_member_force_array = np.concatenate((np.array([' '] * extend_front), raw_member_force_array), axis=0)
         standard_member_force = np.vstack((standard_member_force, raw_member_force_array))
-    return standard_member_force
-
-
-# FILLS IN THE EMPTY SPACES AT THE FRONT OF THE ARRAY  WITH THE REQUIRED DATA. I.E, START EACH ROW WITH MEMBER NAME
-def filled_member_force_array():
-    all_member_force = member_force_array()
+    all_member_force = np.copy(standard_member_force)
+    all_member_force[:] = standard_member_force[:]
     for rows in range(all_member_force.shape[0]):
         if all_member_force[rows:rows + 1, 0:1] == " " and all_member_force[rows:rows + 1, 1:2] == " ":
             all_member_force[rows, 0] = all_member_force[rows - 1, 0]
@@ -39,90 +60,60 @@ def filled_member_force_array():
             all_member_force[rows, 0] = all_member_force[rows - 1, 0]
         else:
             all_member_force = all_member_force
-    return np.delete(all_member_force, 0, axis=0)
-
-
-# GENERATE A LIST OF BEAM NAMES IN ARRAY TO USE AS DICTIONARY KEYS BASED ON USER INPUT
-def get_beam_names():
-    beam_array = member_force_array()[:, [0]]
-    beam = [np.array(b).tolist() for b in beam_array if not b == ' ']
+    name_array = standard_member_force[:, [0, 1]]
+    beam = [np.array(b).tolist() for b in name_array[:, [0]] if not b == ' ']
     beam = [b for sub_b in beam for b in sub_b]
+    load = [np.array(l).tolist() for l in name_array[:, [1]] if not l == ' ']
+    load = [l for sub_l in load for l in sub_l][0:lc_count]
     if beam_option == 1:
-        beam = beam
+        pass
     elif beam_option == 2:
-        starts_with = 'B40102'
-        beam = [b for b in beam if b.startswith(starts_with)]
+        beam_starts_with = 'COL1001'
+        beam = [b for b in beam if b.startswith(beam_starts_with)]
     elif beam_option == 3:
         beam = ['B4010011']
-    else:
-        print('no option selected')
-    return beam
-
-
-# GENERATE A LIST OF LOAD COMBINATION NAMES IN ARRAY TO USE AS DICTIONARY KEYS BASED ON USER INPUT
-def get_load_names():
-    load_array = member_force_array()[:, [1]]
-    load = [np.array(l).tolist() for l in load_array if not l == ' ']
-    load = [l for sub_l in load for l in sub_l][0:lc_count]
     if load_option == 1:
-        load = load
+        pass
     elif load_option == 2:
-        starts_with = '4D+L-'
-        load = [l for l in load if l.startswith(starts_with)]
+        load_starts_with = '4D+L-'
+        load = [l for l in load if l.startswith(load_starts_with)]
     elif load_option == 3:
         load = ['4D+L-SSE']
-    else:
-        print('no option selected')
-    return load
+    np.delete(all_member_force, 0, axis=0)
+    return all_member_force, beam, load, truss_member
 
 
-# allows me to search for any key in a tuple
-class TupleDict(dict):
-    def __contains__(self, key):
-        if super(TupleDict, self).__contains__(key):
-            return True
-        return any(key in k for k in self)
+def requested_member_force_array(joint, beam_option, load_option, file_list):
+    """
+    Builds a dictionary key / value pairs which match the beam and load criteria requested by the user.
+    Generates a second dictionary based on the joint specification(all, start, end)
 
+        Parameters:
+            None yet - will discuss beam option and load options
 
-# SEARCH THROUGH DICTIONARY TO FIND KEYS THAT MATCH BEAM, LOAD, AND JOINT CRITERIA
-def requested_member_force_array():
-    member_force_array_list = filled_member_force_array()
+        Returns:
+             output (dict):     dictionary of key / value pairs which match all beam, load, and joint criteria
+                                specified by the user
+    """
+    member_force_array_list, user_beams, user_loads, truss_member = member_force_array(beam_option, load_option, file_list)
     key = member_force_array_list[0:, [0, 1, 2]]
     beam_names = member_force_array_list[0:, [0]].tolist()
     joint_names = member_force_array_list[0:, [2]]
     value = member_force_array_list[0:, [3, 4, 5, 6, 7, 8]].tolist()
     d = {}
     d_joints = {}
-    for row in range(len(key)):
+    for row, item in enumerate(key):
         d[(tuple(key[row]))] = value[row]
         if ''.join(beam_names[row]) in d_joints:
             pass
         else:
             d_joints[''.join(beam_names[row])] = [joint_names[row][0], joint_names[row + 1][0]]
     d = TupleDict(d)
-    output_keys = []*3
-    output_values = []*6
-    user_beams = get_beam_names()
-    user_loads = get_load_names()
+    output = {}
     for beams in user_beams:
-        if beams in d and joint == 'ALL':
-            for loads in user_loads:
-                if g_mem.truss_member:
-                    output_keys.append([beams, loads, d_joints[beams][0]])
-                    output_values.append(d[(beams, loads, d_joints[beams][0])])
-                else:
-                    i = 0
-                    while i < 2:
-                        output_keys.append([beams, loads, d_joints[beams][i]])
-                        output_values.append(d[(beams, loads, d_joints[beams][i])])
-                        i += 1
-        elif beams in d and joint == 'START':
-            for loads in user_loads:
-                output_keys.append([beams, loads, d_joints[beams][0]])
-                output_values.append(d[(beams, loads, d_joints[beams][0])])
-        elif beams in d and joint == 'END':
-            for loads in user_loads:
-                output_keys.append([beams, loads, d_joints[beams][1]])
-                output_values.append(d[(beams, loads, d_joints[beams][1])])
-    output_array = np.hstack((np.array(output_keys), np.array(output_values)))
-    return output_array
+        indices = [0] if joint == 'START' or truss_member else [1] if joint == 'END' else range(len(d_joints[beams]))
+        for loads in user_loads:
+            for index in indices:
+                t = (beams, loads, d_joints[beams][index])
+                output[t] = d[t]
+    return output
--- generate_mem_array_info.py
+++ generate_mem_array_info.py
@@ -1,40 +1,61 @@
-# READS .GTO FILE AND DETERMINES NUMBER OF OCCURRENCES OF 'LIST FORCES' AND RETURNS A LIST OF ELEMENTS IN THE USER
-# SPECIFIED LIST FORCES SET. ALSO CALCULATED NUMBER OF MEMBERS AND NUMBER OF LOAD COMBINATIONS IN THE SET
-
-input_file = 'RB Steel Framing_v34.gto'
-member_set = 0      # based on input from user
-increment = 0
-blanks = 0
-next_line = 0
-trigger_string = 'LIST FOR'
+import numpy as np
 
 
-with open(input_file, 'r') as f:
-    file_list = []
-    for lines in f:
-        file_list.append(lines.rstrip())
+def get_force_positions(file_list):
+    """
+    Searches the input file for trigger. Returns a list of lines in input file which contain the strings
+    and a list of the index (line number) for each of the lines containing the trigger.
+            Parameters:
+                None yet
+
+            Triggers:
+                'LIST FOR': Obtains line which lists member forces
+                'OTHER TRIGGERS': Reserved for future triggers / expansion (i.e, displacements, reactions, etc.)
+    """
+    trigger_string = 'LIST FOR'
+    member_set = 2  # based on input from user
+    result = [v for v in file_list if trigger_string in v]
+    result_index = [index for index, value in enumerate(result)]
+    index_list = [file_list.index(result[i]) for i in range(len(result))]
+    result = [v[v.find(trigger_string):] for v in result]
+    return index_list[member_set], result
 
 
-result = [v for v in file_list if trigger_string in v]
-index_list = [file_list.index(result[i]) for i in range(len(result))]
-first_useful_line = index_list[member_set] + 24
+def get_member_force_list_info(file_list):
+    """
+    Determines the index (line number) of first useful line and last useful line of the input file which contains the
+    block of requested data. Uses the size of the block to calculate the number of blank lines
+    (spaces between subsets of data), the number of members in the requested data set, whether the data set contains
+    truss type members (or any members that do not have moments), and the number of load combinations for each subset
+    of the data.
 
-for i in range(len(file_list)):
-    if not file_list[first_useful_line + next_line].startswith('1'):
-        next_line += 1
-    else:
-        end_index = first_useful_line + next_line
-        break
+        Parameters:
+            None yet
 
-blanks = file_list[first_useful_line:end_index].count("")
-mem_num = blanks + 1
-member_forces = list(filter(None, file_list[first_useful_line:end_index]))
-
-
-def load_comb_count():
-    if not any('MOMENT' in st for st in file_list[index_list[member_set]:first_useful_line - 2]):
+        Returns:
+             member_results (array):    a 1d array of the member results
+             lc_count (int):            the number of load combinations
+             truss_member (bool):      truss member type (True or False)
+             mem_num (int):             the number of members in the data block
+             first_useful_line (int):   index of start of requested data block
+             end_index (int):           index of end of requested data block
+    """
+    index_for_mem_set, ph_1 = get_force_positions(file_list)
+    first_useful_line = index_for_mem_set + 24
+    next_line = 0
+    for i, line in enumerate(file_list):
+        if not file_list[first_useful_line + next_line].startswith('1'):
+            next_line += 1
+        else:
+            end_index = first_useful_line + next_line
+            break
+    blanks = file_list[first_useful_line:end_index].count("")
+    mem_num = blanks + 1
+    member_forces = list(filter(None, file_list[first_useful_line:end_index]))
+    truss_member = False
+    if not any('MOMENT' in st for st in file_list[index_for_mem_set:first_useful_line - 2]):
         lc_count = int((end_index - first_useful_line - blanks) / mem_num)
         truss_member = True
     else:
-        lc_count = int((end_index - first_useful_line - blanks) / (mem_num*2))
-    return lc_count
+        lc_count = int((end_index - first_useful_line - blanks) / (mem_num * 2))
+    return np.array(member_forces), lc_count, truss_member, mem_num, end_index, first_useful_line
