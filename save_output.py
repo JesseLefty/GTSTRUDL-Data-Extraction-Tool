7b2763b96585c4327bb0f21a2b5f8973502abac6
JesseLefty <93848963+JesseLefty@users.noreply.github.com>
2022-01-28 21:54:12 -0600

    Error Handling (#5)
    * Error Handling
    
    See change log for detail.
    
    Made changes to GUI in the form of error pop-ups
    
    Changed how member forces were extracted to use built in python lists rather than numpy.
    
    Added ability to store results sets as .json file
    
    * Update change_log.txt

--- save_output.py
+++ save_output.py
@@ -1,48 +1,83 @@
+import Error_Handling
 import extract_member_forces as emf
 import generate_mem_array_info as g_mem
 import csv
 import openpyxl
+from tkinter import *
+from tkinter.ttk import *
 
 
-def run_program(output_file_name, joint, member_set, out_format, input_file, file_as_list, beam_id, load_id):
-    """
-        Runs the full program. Saves all required outputs as selected by the user and saves either an xlsx file or a
-        csv file.
+class RunProgram:
 
-            Parameters:
+    def __init__(self, initial_window):
+        self.initial_window = initial_window
+        pass
 
-            Returns:
-                 .xlsx or .csv file of requested user inputs
+    member_set_errors = []
+    list_errors = []
+
+    def run_program(self, output_file_name, joint, member_set, out_format, input_file, file_as_list, beam_id, load_id):
         """
-    with open(output_file_name, 'w', newline='') as w:
-        if out_format:
+            Runs the full program. Saves all required outputs as selected by the user and saves either an xlsx file or a
+            csv file.
+
+                Parameters:
+
+                Returns:
+                     .xlsx or .csv file of requested user inputs
+            """
+        self.member_set_errors.clear()
+        self.list_errors.clear()
+        if out_format == '.xlsx':
             wb = openpyxl.Workbook()
-            for item in range(len(member_set)):
-                member_forces, load_comb_count, truss_member, mem_num, end_index, first_useful_line = \
-                    g_mem.ParseFileForData(item, input_file, member_set).get_member_force_list_info(file_as_list)
-                sheet_name = 'Load Set ' + str(item)
-                member_forces = emf.GenerateOutputArray(joint, item).requested_member_force_array(member_forces,
-                                                                                                  load_comb_count,
-                                                                                                  truss_member, beam_id,
-                                                                                                  load_id)
+            for items in range(len(member_set)):
+                member_forces, load_comb_count, truss_member, mem_num, end_index, first_useful_line \
+                    = g_mem.ParseFileForData(items, input_file, member_set).get_member_force_list_info(file_as_list)
+                member_forces = emf.GenerateOutputArray(joint, items, member_forces, load_comb_count, truss_member,
+                                                        beam_id, load_id).requested_member_force_array()
+                sheet_name = 'Load Set ' + str(items + 1)
                 sheet = wb.create_sheet(f'{sheet_name}')
                 d_list = [list(k) + v for k, v in member_forces.items()]
+
+                if len(member_forces) == 0:
+                    self.member_set_errors.append(items + 1)
+
+                error_keys = [k for k in member_forces.keys()]
+                if {'Beam Error'}.issubset(error_keys) or {'Load Error'}.issubset(error_keys):
+                    self.list_errors.append(items + 1)
+
                 for row, key in enumerate(member_forces.items(), start=1):
-                    sheet[f"A{row}"] = (' '.join(d_list[row - 1])).replace(' ', ',')
+                    for col, key in enumerate(d_list[row - 1], start=1):
+                        sheet.cell(column=col, row=row, value=d_list[row-1][col-1])
             del wb['Sheet']
-            wb.save(f'{output_file_name}.xlsx')
+            wb.save(output_file_name)
         else:
-            with open(output_file_name + '.csv', 'w', newline='') as w:
-                for item in range(len(member_set)):
+            with open(output_file_name, 'w', newline=''):
+
+                for items in range(len(member_set)):
                     member_forces, load_comb_count, truss_member, mem_num, end_index, first_useful_line = \
-                        g_mem.ParseFileForData(item, input_file, member_set).get_member_force_list_info(file_as_list)
-                    member_forces = emf.GenerateOutputArray(joint, item).requested_member_force_array(member_forces,
-                                                                                                      load_comb_count,
-                                                                                                      truss_member,
-                                                                                                      beam_id, load_id)
-                    with open(output_file_name + '.csv', 'a', newline='') as a:
+                        g_mem.ParseFileForData(items, input_file, member_set).get_member_force_list_info(file_as_list)
+                    member_forces = emf.GenerateOutputArray(joint, items, member_forces, load_comb_count, truss_member,
+                                                            beam_id, load_id).requested_member_force_array()
+
+                    with open(output_file_name, 'a', newline='') as a:
                         csv.writer(a).writerows((list(k) + v for k, v in member_forces.items()))
-                    # print(f'{output_file_name} saved successfully')
-                    # print(f'Number of Load Combinations = {load_comb_count}')
-                    # print(f'Number of Members = {mem_num}')
-                    # print(f'number of rows = {end_index - first_useful_line}')
+
+                    if len(member_forces) == 0:
+                        self.member_set_errors.append(items + 1)
+
+                    error_keys = [k for k in member_forces.keys()]
+                    if {'Beam Error'}.issubset(error_keys) or {'Load Error'}.issubset(error_keys):
+                        self.list_errors.append(items + 1)
+
+        if len(self.member_set_errors) or len(self.list_errors) > 0:
+            error_set_list = list(set(self.list_errors + self.member_set_errors))
+            Error_Handling.ErrorHandling(self.initial_window).item_not_found(error_set_list)
+        else:
+            success_window = Toplevel(self.initial_window)
+            success_window.geometry('200x50')
+            success_window.title('Complete')
+            self.initial_window.eval(f'tk::PlaceWindow {str(success_window)} center')
+            Label(success_window, text='Output Saved Successfully').pack()
+            Button(success_window, text='OK', command=success_window.destroy).pack()
+
