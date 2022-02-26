44b3e9862b1e5343a53f192f2122d6416bcef566
JesseLefty <93848963+JesseLefty@users.noreply.github.com>
2022-02-26 13:07:12 -0600

    Member force bug fixes (#8)
    * Member Force Bug Fixes
    
    Major changes to extract_member_forces.py. Changed member force extraction to use fixed column widths for FX, FY, FZ, MX, MY, MZ.
    
    Minor changes to other included files mainly related to reorganization of project modules and adding docstrings
    See Change log for more detail
    
    * Update change_log.txt
    
    * Member Force Refactor
    
    In some cases the .gto file can contain legitimate warnings. These warnings alter the spacing and formatting of the member results sets when they occur. To resolve this the way the member forces were parsed has been overhauled. The member results set is no longer read line by line. Instead it is read once to split the results into 'blocks' of members. Then each block is checked for warnings and number of load combinations and joints.
    
    * Updated Bug Fixes for Member Forces
    
    Revamped how member forces were extracted from the file and put into the output dictionary.
    
    Errors in user input (i.e., invalid beam or load names) are now reported before they result in a key error while building the dictionary - in doing so by the time the dictionary is built there is no possibility for an incorrect user input and no key error

--- save_output.py
+++ save_output.py
@@ -14,6 +14,8 @@ class RunProgram:
 
     member_set_errors = []
     list_errors = []
+    beam_errors = []
+    load_errors = []
 
     def run_program(self, output_file_name, joint, member_set, out_format, input_file, beam_id, load_id):
         """
@@ -21,12 +23,21 @@ class RunProgram:
             csv file.
 
                 Parameters:
+                    output_file_name (str):     name of file in which to save the data
+                    joint (tuple):              list containing "ALL", "STA", or "END" for joint specification
+                    member_set (list):          list of values corresponding to the index of requested member set
+                    out_format (str):           .csv or .xlsx
+                    input_file (str):           input file in which to sear for data
+                    beam_id (list):             list containing tuples of beam spec (int), and user beam input (str)
+                    load_id (list):             list containing tuples of load spec (int), and user load input (str)
 
                 Returns:
                      .xlsx or .csv file of requested user inputs
             """
         self.member_set_errors.clear()
         self.list_errors.clear()
+        self.beam_errors.clear()
+        self.load_errors.clear()
         if out_format == '.xlsx':
             wb = openpyxl.Workbook()
             for items in range(len(member_set)):
@@ -37,12 +48,13 @@ class RunProgram:
                 sheet_name = 'Load Set ' + str(items + 1)
                 sheet = wb.create_sheet(f'{sheet_name}')
                 d_list = [list(k) + v for k, v in member_forces.items()]
-
                 if len(member_forces) == 0:
                     self.member_set_errors.append(items + 1)
 
-                if errors:
+                if errors[0] or errors[1]:
                     self.list_errors.append(items + 1)
+                    self.beam_errors.append(errors[0])
+                    self.load_errors.append(errors[1])
 
                 for row, key in enumerate(member_forces.items(), start=1):
                     for col, key in enumerate(d_list[row - 1], start=1):
@@ -51,7 +63,6 @@ class RunProgram:
             wb.save(output_file_name)
         else:
             with open(output_file_name, 'w', newline=''):
-
                 for items in range(len(member_set)):
                     member_forces, end_index, first_useful_line = \
                         g_mem.ParseFileForData(items, input_file, member_set).get_member_force_list_info()
@@ -64,16 +75,18 @@ class RunProgram:
                     if len(member_forces) == 0:
                         self.member_set_errors.append(items + 1)
 
-                    if errors:
+                    if errors[0] or errors[1]:
                         self.list_errors.append(items + 1)
+                        self.beam_errors.append(errors[0])
+                        self.load_errors.append(errors[1])
 
         if len(self.member_set_errors) or len(self.list_errors) > 0:
             error_set_list = list(set(self.list_errors + self.member_set_errors))
-            error_handling.ErrorHandling(self.initial_window).item_not_found(error_set_list)
-            error_handling.ErrorHandling(self.initial_window).missing_key(errors)
+            error_handling.ErrorHandling(self.initial_window).item_not_found(error_set_list, self.beam_errors,
+                                                                             self.load_errors)
         else:
             success_window = Toplevel(self.initial_window)
-            success_window.geometry('200x50')
+            success_window.geometry('200x120')
             success_window.title('Complete')
             success_window.resizable(False, False)
             success_window.grab_set()
