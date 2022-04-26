35946895a9730089db81f9b4435527a20018afc1
JesseLefty <jesselefty88@gmail.com>
2022-04-25 21:53:15 -0500

    Consolidated code related to parsing file for input data
    Combined the three independent 'generate array info' files into a single file. The code was nearly an identical copy for each file. Adjusted save_output file to pull from one location. No changes to functionality, just code cleanup.

--- Tool & Configuration/config.py
+++ Tool & Configuration/config.py
@@ -7,7 +7,6 @@ rb_options = {1: '',
               4: 'Contains: ',
               5: 'List: '}
 
-
 requested_results_headings = {'Member Force':
                                   {'headings': ['Set #', 'Set Name', 'Joint Spec.', 'Name Spec.', 'Load Spec.'],
                                    'column width': [40, 200, 100, 100, 100],
@@ -24,3 +23,17 @@ requested_results_headings = {'Member Force':
                                    'text location': ['center', 'w', 'center', 'w', 'w', 'w'],
                                    'text label': ['MEMBER NAME(s)', 'PROFILE(s)', 'IR RANGE']}
                               }
+
+result_configuration_parameters = {'Member Force':
+                                       {'Trigger String': 'LIST FOR',
+                                        'Skip Lines': 22,
+                                        'End Trigger': '1'},
+                                   'Joint Reaction':
+                                       {'Trigger String': 'RESULTANT JOINT LOADS SUPPORTS',
+                                        'Skip Lines': 3,
+                                        'End Trigger': '1'},
+                                   'Code Check':
+                                       {'Trigger String': 'DESIGN TRACE OUTPUT',
+                                        'Skip Lines': 13,
+                                        'End Trigger': 'END OF TRACE OUTPUT'}
+                                   }
