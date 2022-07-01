74da5c5e82a87939c852e1edfa54ef768fc8dcea
JesseLefty <93848963+JesseLefty@users.noreply.github.com>
2022-06-30 21:47:06 -0500

    Bug fixes based on testing new .gto file (#33)
    Fixed bug where if 'output by member' was the very first line of the file an error would be returned because the line number was 0 (or 'none' or 'false') thus showing up as not existing. Changed code to validate that the string_position is not None
    
    added splash screen to executable with text for loading and 'complete' based on a sleep time.
    
    updated user guide to latest version
    
    added splash image file
    
    fixed error where if there was a warning message at the top of the code check the warning message would be displayed instead of the input line number. There were no issues with the actual operation of the program, just the incorrect input file line was displayed in the available results window. This was solved by finding the 'trigger_string' and then looping backwards through the input file from that point until the program found '{' indicating the correct input line number.

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from Tools.data_storage import ResultsParameters
 from Tools import shared_stuff
 from GUI.landing_window import FirstWindow
 
+import time
+try:
+    import pyi_splash
+    time.sleep(2)
+    pyi_splash.update_text('Complete')
+    time.sleep(1)
+    pyi_splash.close()
+
+except:
+    pass
+
 if __name__ == '__main__':
     shared_stuff.data_store = ResultsParameters()
     initial_window = Tk()
