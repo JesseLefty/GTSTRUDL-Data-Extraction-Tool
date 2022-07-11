8f396eb2fe5956af55895e40f51906ad962d6dfd
JesseLefty <jesselefty88@gmail.com>
2022-07-11 08:07:55 -0500

    Update check_input_errors.py
    Created standalone methods for sections of code that were repeated multiple times.
    
    Added method for checking if a user criteria is valid, returns true if it's valid and false if it is not.
    
    Added method for returning a list of invalid user criteria
    
    Replaced the duplicated code with the two previously described methods.

--- DataProcessing/check_input_errors.py
+++ DataProcessing/check_input_errors.py
@@ -10,6 +10,54 @@ from Tools import shared_stuff
 from error_handling import ErrorHandling
 
 
+def is_user_criteria_valid(user_choice: int, available_results: list, user_criteria: str) -> bool:
+    """
+    Takes the user choice and available results list and returns a parsed list containing only the results that match
+    the user choice criteria
+
+    :param user_choice: int representing the user choice
+    :param available_results: list of all available results
+    :param user_criteria: str containing the user requested criteria
+
+    :return: True if available results contains matching user criteria, else False
+    """
+    user_criteria_up = user_criteria.upper()
+    if user_choice == 1:
+        return True
+    elif user_choice == 2:
+        return any(True for item in available_results if item.startswith(user_criteria_up))
+    elif user_choice == 3:
+        return any(True for item in available_results if item.endswith(user_criteria_up))
+    elif user_choice == 4:
+        return any(True for item in available_results if user_criteria_up in item)
+    elif user_choice == 5:
+        name_list = "".join(user_criteria_up).replace(" ", "").split(',')
+        for item in name_list:
+            if item not in available_results:
+                return False
+        return True
+
+
+def get_invalid_results(user_choice: int, available_results: list, user_criteria: str) -> list:
+    """
+    returns a list of invalid user criteria
+
+    :param user_choice: int representing the user choice
+    :param available_results: list of all available results
+    :param user_criteria: str containing the user requested criteria
+
+    :return: list containing invalid user criteria
+    """
+    user_criteria_up = user_criteria.upper()
+    if user_choice == 1:
+        return []
+    elif 1 < user_choice < 5:
+        return [user_criteria_up]
+    elif user_choice == 5:
+        name_list = "".join(user_criteria_up).replace(" ", "").split(',')
+        return [item for item in name_list if item not in available_results]
+
+
 class FindInputErrors:
 
     """
@@ -22,7 +70,7 @@ class FindInputErrors:
         self.results = shared_stuff.data_store
         self.results.tab_name = tab_name
         self.name_id = self.results.name
-        self.num_of_sets = self.results.set_index
+        self.num_of_sets = len(self.results.set_index)
         self.error_dict = {}
 
     def is_input_error(self, initial_window):
@@ -32,7 +80,7 @@ class FindInputErrors:
         :return: True if error exists, False if no error
         """
         if self.tab_name in ['Member Force', 'Joint Reaction']:
-            for num in range(len(self.num_of_sets)):
+            for num in range(self.num_of_sets):
                 self.find_error(num)
             if self.error_dict.keys():
                 sets_with_errors = self.error_dict.keys()
@@ -43,7 +91,7 @@ class FindInputErrors:
             else:
                 return False
         elif self.tab_name == 'Code Check':
-            for num in range(len(self.num_of_sets)):
+            for num in range(self.num_of_sets):
                 self.code_errors(num)
             if self.error_dict.keys():
                 sets_with_errors = self.error_dict.keys()
@@ -59,11 +107,11 @@ class FindInputErrors:
     def find_error(self, set_num):
         """
         Checks user member and joint reaction inputs for errors and stores the invalid input in error dictionary
+
         :param set_num: user result set number
         """
         names = None
         loads = None
-        load_id = self.results.load
         extracted_result_list, _, _ = ParseFileForData(set_num, self.tab_name).get_result_list_info()
         if self.tab_name == 'Member Force':
             _, names, loads = emf.GenerateOutputArray(self.tab_name, set_num, extracted_result_list).member_force_array()
@@ -72,75 +120,20 @@ class FindInputErrors:
         flat_names = [item for sub_list in names for item in sub_list]
         flat_loads = [item for sub_list in loads for item in sub_list]
         name_choice = self.name_id[set_num][0]
-        load_choice = load_id[set_num][0]
-        name_error = []
-        load_error = []
-        set_num_error = []
-        error = False
-        if name_choice == 1:
-            pass
-        elif name_choice == 2:
-            name_starts_with = self.name_id[set_num][1]
-            if not any(item.startswith(name_starts_with) for item in flat_names):
-                name_error = name_starts_with
-                set_num_error = set_num + 1
-            else:
-                pass
-        elif name_choice == 3:
-            name_ends_with = self.name_id[set_num][1]
-            if not any(item.endswith(name_ends_with) for item in flat_names):
-                name_error = name_ends_with
-                set_num_error = set_num + 1
-            else:
-                pass
-        elif name_choice == 4:
-            name_contains = self.name_id[set_num][1]
-            if not any(name_contains in item for item in flat_names):
-                name_error = name_contains
-                set_num_error = set_num + 1
-        elif name_choice == 5:
-            name_text = self.name_id[set_num][1].upper()
-            name_list = "".join(name_text).replace(" ", "").split(',')
-            for item in name_list:
-                if item not in flat_names:
-                    name_error.append(item)
-                    error = True
-                else:
-                    error = False
-            if error:
-                set_num_error = set_num + 1
-        if load_choice == 1:
-            pass
-        elif load_choice == 2:
-            load_starts_with = load_id[set_num][1]
-            if not any(item.startswith(load_starts_with) for item in flat_loads):
-                load_error = load_starts_with
-                set_num_error = set_num + 1
-            else:
-                pass
-        elif load_choice == 3:
-            load_ends_with = load_id[set_num][1]
-            if not any(item.endswith(load_ends_with) for item in flat_loads):
-                load_error = load_ends_with
-                set_num_error = set_num + 1
-            else:
-                pass
-        elif load_choice == 4:
-            load_contains = load_id[set_num][1]
-            if not any(load_contains in item for item in flat_loads):
-                load_error = load_contains
-                set_num_error = set_num + 1
-        elif load_choice == 5:
-            load_text = load_id[set_num][1].upper()
-            load_list = "".join(load_text).replace(" ", "").split(',')
-            for item in load_list:
-                if item not in flat_loads:
-                    load_error.append(item)
-                    error = True
-                else:
-                    error = False
-                if error:
-                    set_num_error = set_num + 1
+        name_spec = self.name_id[set_num][1]
+        load_choice = self.results.load[set_num][0]
+        load_spec = self.results.load[set_num][1]
+        set_num_error = False
+        if not is_user_criteria_valid(name_choice, flat_names, name_spec):
+            name_error = get_invalid_results(name_choice, flat_names, name_spec)
+            set_num_error = set_num + 1
+        else:
+            name_error = False
+        if not is_user_criteria_valid(load_choice, flat_loads, load_spec):
+            load_error = get_invalid_results(load_choice, flat_loads, load_spec)
+            set_num_error = set_num + 1
+        else:
+            load_error = False
         if set_num_error:
             self.error_dict[set_num + 1] = (name_error, load_error)
         else:
@@ -149,94 +142,43 @@ class FindInputErrors:
     def code_errors(self, set_num):
         """
         Checks user code check inputs for errors and returns error dictionary of invalid items
+
         :param set_num:
         :return: user result set number
         """
         extracted_result_list, _, _ = ParseFileForData(set_num, self.tab_name).get_result_list_info()
         code_check_list = ecc.GenerateOutputArray(self.tab_name, set_num, extracted_result_list).code_check_array()
-        profile = self.results.profile
-        ir_range = self.results.ir_range
         name_col = []
         profile_col = []
         ir_col = []
-        name_error = []
-        profile_error = []
         ir_error = []
         set_num_error = []
-        error = False
         for row in code_check_list:
             name_col.append(row[0])
             profile_col.append(row[11])
             ir_col.append(row[5])
         name_choice = self.name_id[set_num][0]
-        profile_choice = profile[set_num][0]
-        ir_choice = ir_range[set_num][0]
-        if name_choice == 1:
-            pass
-        elif name_choice == 2:
-            name_starts_with = self.name_id[set_num][1]
-            if not any(item.startswith(name_starts_with) for item in name_col):
-                name_error = name_starts_with
-                set_num_error = set_num + 1
-            else:
-                pass
-        elif name_choice == 3:
-            name_ends_with = self.name_id[set_num][1]
-            if not any(item.startswith(name_ends_with) for item in name_col):
-                name_error = name_ends_with
-                set_num_error = set_num + 1
-            else:
-                pass
-        elif name_choice == 4:
-            name_contains = self.name_id[set_num][1]
-            if not any(name_contains in item for item in name_col):
-                name_error = name_contains
-                set_num_error = set_num + 1
-        elif name_choice == 5:
-            name_text = self.name_id[set_num][1].upper()
-            name_list = "".join(name_text).replace(" ", "").split(',')
-            for item in name_list:
-                if item not in name_col:
-                    name_error.append(item)
-                    error = True
-                else:
-                    error = False
-            if error:
-                set_num_error = set_num + 1
-        if profile_choice == 1:
-            pass
-        elif profile_choice == 2:
-            profile_starts_with = profile[set_num][1]
-            if not any(item.startswith(profile_starts_with) for item in profile_col):
-                profile_error = profile_starts_with
-                set_num_error = set_num + 1
-            else:
-                pass
-        elif profile_choice == 3:
-            profile_ends_with = profile[set_num][1]
-            if not any(item.startswith(profile_ends_with) for item in profile_col):
-                profile_error = profile_ends_with
-                set_num_error = set_num + 1
-            else:
-                pass
-        elif profile_choice == 4:
-            profile_contains = profile[set_num][1]
-            if not any(profile_contains in item for item in profile_col):
-                profile_error = profile_contains
-                set_num_error = set_num + 1
-        elif profile_choice == 5:
-            profile_text = profile[set_num][1].upper()
-            profile_list = "".join(profile_text).replace(" ", "").split(',')
-            for item in profile_list:
-                if item not in profile_col:
-                    profile_error.append(item)
-                    error = True
-                else:
-                    error = False
-            if error:
-                set_num_error = set_num + 1
+        name_spec = self.name_id[set_num][1]
+        profile_choice = self.results.profile[set_num][0]
+        profile_spec = self.results.profile[set_num][1]
+        ir_choice = self.results.ir_range[set_num][0]
+        ir_max = self.results.ir_range[set_num][1][1]
+        ir_min = self.results.ir_range[set_num][1][0]
+
+        if not is_user_criteria_valid(name_choice, name_col, name_spec):
+            name_error = get_invalid_results(name_choice, name_col, name_spec)
+            set_num_error = set_num + 1
+        else:
+            name_error = False
+
+        if not is_user_criteria_valid(profile_choice, profile_col, profile_spec):
+            profile_error = get_invalid_results(profile_choice, profile_col, profile_spec)
+            set_num_error = set_num + 1
+        else:
+            profile_error = False
+
         try:
-            [float(item) for item in ir_range[set_num][1] if not item == '']
+            [float(item) for item in self.results.ir_range[set_num][1] if not item == '']
             not_number = False
         except ValueError:
             not_number = True
@@ -247,20 +189,20 @@ class FindInputErrors:
             if ir_choice == 1:
                 pass
             elif ir_choice == 2:
-                ir_less_than = float(ir_range[set_num][1][1])
+                ir_less_than = float(ir_max)
                 user_ir = [p for p in ir_col if float(p) < ir_less_than]
                 if not user_ir:
                     ir_error = f'< {ir_less_than}'
                     set_num_error = set_num + 1
             elif ir_choice == 3:
-                ir_greater_than = float(ir_range[set_num][1][0])
+                ir_greater_than = float(ir_min)
                 user_ir = [p for p in ir_col if float(p) > ir_greater_than]
                 if not user_ir:
                     ir_error = f'> {ir_greater_than}'
                     set_num_error = set_num + 1
             elif ir_choice == 4:
-                lower_ir = float(ir_range[set_num][1][0])
-                upper_ir = float(ir_range[set_num][1][1])
+                lower_ir = float(ir_min)
+                upper_ir = float(ir_min)
                 if lower_ir > upper_ir or (lower_ir or upper_ir) < 0:
                     ir_error = 'Invalid IR range'
                     set_num_error = set_num + 1
